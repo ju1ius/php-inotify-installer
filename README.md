33f825e035b3524b0452f1978906f1117f521ec5
ju1ius <ju1ius@ju1ius-sidbang.(none)>
2013-08-30 00:14:47 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 php-inotify-installer
 =====================
 
-Just a few script to ease the installation of the inotify extension on Debian
+Little script to ease the installation of the inotify extension on Debian.
+
+```sh
+# Install the dependencies
+sudo aptitude install tar curl php-dev checkinstall
+
+# build...
+cd /path/to/php-inotify-installer
+./build.sh
+
+# and install
+sudo dpkg -i ./php5-inotify_*.deb
+```
---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+__FILE__=$(readlink -f "$0")
+__DIR__=$(dirname "$__FILE__")
+
+pushd $__DIR__
+
+# remove previous builds
+rm -r inotify-*/
+
+# if we don't have a source tarball, download it
+if [[ ! -f $(echo inotify-*.tgz) ]]
+then
+    curl -J -O http://pecl.php.net/get/inotify
+    [[ $? -ne 0 ]] && exit 1
+fi
+
+src_tar=$(find . -maxdepth 1 -name "inotify-*.tgz" | sort -Vr | head -1)
+
+# extract source tarball
+tar -zxvf "$src_tar"
+
+src_dir=$(find . -maxdepth 1 -type d -name "inotify-*" | sort -Vr | head -1)
+
+# copy post{install,remove} scripts
+cp *-pak $src_dir
+
+# build everything
+pushd "$src_dir"
+
+phpize
+[[ $? -ne 0 ]] && exit 1
+
+./configure
+[[ $? -ne 0 ]] && exit 2
+
+make
+[[ $? -ne 0 ]] && exit 3
+make test
+[[ $? -ne 0 ]] && exit 4
+
+sudo checkinstall \
+    --install=no \
+    --pkgname php5-inotify \
+    --provides php5-inotify \
+    --requires php5-common \
+    --deldoc
+[[ $? -ne 0 ]] && exit 5
+
+# cleanup after successfull install
+mv *.deb "$__DIR__"
+popd
+sudo rm -rf inotify-*/
+rm package.xml
+exit 0
